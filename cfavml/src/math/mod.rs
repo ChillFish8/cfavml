f3e1ef089c166d487d5494e303a29fd0bbd006cf
ChillFish8 <hburt2003@gmail.com>
2024-07-09 17:51:49 +0100

    Add fast math back

--- cfavml/src/math/mod.rs
+++ cfavml/src/math/mod.rs
@@ -1,8 +1,15 @@
 mod default;
+#[cfg(feature = "nightly")]
+mod fast_math;
 
 pub use default::StdMath;
+#[cfg(feature = "nightly")]
+pub use fast_math::FastMath;
 
+#[cfg(not(feature = "nightly"))]
 pub type AutoMath = StdMath;
+#[cfg(feature = "nightly")]
+pub type AutoMath = FastMath;
 
 /// Core simple math operations that can be adjusted for certain features
 /// or architectures.
