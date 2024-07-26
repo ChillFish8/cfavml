588748d208407b2eb3bd2780f57bce4dbdebbd68
ChillFish8 <hburt2003@gmail.com>
2024-07-26 19:01:16 +0100

    Make transpose op generic

---
+++ cfavml-gemm/src/transpose/impl_avx2.rs
@@ -0,0 +1,189 @@
+use std::arch::x86_64::*;
+
+use cfavml::danger::*;
+
+use crate::transpose::{generic_transpose, TransposeMatrix};
+
+#[inline]
+#[target_feature(enable = "avx2")]
+/// Performs a matrix transposition on 32 bit values.
+///
+/// # Safety
+///
+/// The size of the input and output buffers _must_ be equal to the calculated size by doing
+/// `width * height`.
+///
+/// This function also assumes `avx2` CPU features are available.
+pub unsafe fn f32_xany_avx2_nofma_transpose(
+    width: usize,
+    height: usize,
+    data: &[f32],
+    result: &mut [f32],
+) {
+    generic_transpose::<f32, Avx2>(width, height, data, result)
+}
+
+#[inline]
+#[target_feature(enable = "avx2")]
+/// Performs a matrix transposition on 64 bit values.
+///
+/// # Safety
+///
+/// The size of the input and output buffers _must_ be equal to the calculated size by doing
+/// `width * height`.
+///
+/// This function also assumes `avx2` CPU features are available.
+pub unsafe fn f64_xany_avx2_nofma_transpose(
+    width: usize,
+    height: usize,
+    data: &[f64],
+    result: &mut [f64],
+) {
+    generic_transpose::<f64, Avx2>(width, height, data, result)
+}
+
+impl TransposeMatrix<f32> for Avx2 {
+    type RegisterMatrix = DenseLane<Self::Register>;
+
+    #[inline(always)]
+    unsafe fn load_matrix(
+        offset: usize,
+        width: usize,
+        data_ptr: *const f32,
+    ) -> Self::RegisterMatrix {
+        DenseLane {
+            a: Self::load(data_ptr.add(offset)),
+            b: Self::load(data_ptr.add(offset + (width * 1))),
+            c: Self::load(data_ptr.add(offset + (width * 2))),
+            d: Self::load(data_ptr.add(offset + (width * 3))),
+            e: Self::load(data_ptr.add(offset + (width * 4))),
+            f: Self::load(data_ptr.add(offset + (width * 5))),
+            g: Self::load(data_ptr.add(offset + (width * 6))),
+            h: Self::load(data_ptr.add(offset + (width * 7))),
+        }
+    }
+
+    #[inline(always)]
+    unsafe fn write_matrix(
+        offset: usize,
+        height: usize,
+        matrix: Self::RegisterMatrix,
+        result_ptr: *mut f32,
+    ) {
+        Self::write(result_ptr.add(offset), matrix.a);
+        Self::write(result_ptr.add(offset + (1 * height)), matrix.b);
+        Self::write(result_ptr.add(offset + (2 * height)), matrix.c);
+        Self::write(result_ptr.add(offset + (3 * height)), matrix.d);
+        Self::write(result_ptr.add(offset + (4 * height)), matrix.e);
+        Self::write(result_ptr.add(offset + (5 * height)), matrix.f);
+        Self::write(result_ptr.add(offset + (6 * height)), matrix.g);
+        Self::write(result_ptr.add(offset + (7 * height)), matrix.h);
+    }
+
+    #[inline(always)]
+    unsafe fn transpose_register_matrix(
+        matrix: Self::RegisterMatrix,
+    ) -> Self::RegisterMatrix {
+        let temp = DenseLane {
+            // Unpack the low elements of the dense lane
+            a: _mm256_unpacklo_ps(matrix.a, matrix.b),
+            b: _mm256_unpacklo_ps(matrix.c, matrix.d),
+            c: _mm256_unpacklo_ps(matrix.e, matrix.f),
+            d: _mm256_unpacklo_ps(matrix.g, matrix.h),
+
+            // Unpack the high elements of the dense lane
+            e: _mm256_unpackhi_ps(matrix.a, matrix.b),
+            f: _mm256_unpackhi_ps(matrix.c, matrix.d),
+            g: _mm256_unpackhi_ps(matrix.e, matrix.f),
+            h: _mm256_unpackhi_ps(matrix.g, matrix.h),
+        };
+
+        let half_transposed = DenseLane {
+            a: _mm256_shuffle_ps::<0x44>(temp.a, temp.b),
+            b: _mm256_shuffle_ps::<0xEE>(temp.a, temp.b),
+            c: _mm256_shuffle_ps::<0x44>(temp.c, temp.d),
+            d: _mm256_shuffle_ps::<0xEE>(temp.c, temp.d),
+            e: _mm256_shuffle_ps::<0x44>(temp.e, temp.f),
+            f: _mm256_shuffle_ps::<0xEE>(temp.e, temp.f),
+            g: _mm256_shuffle_ps::<0x44>(temp.g, temp.h),
+            h: _mm256_shuffle_ps::<0xEE>(temp.g, temp.h),
+        };
+
+        DenseLane {
+            a: _mm256_permute2f128_ps::<0x20>(half_transposed.a, half_transposed.c),
+            b: _mm256_permute2f128_ps::<0x20>(half_transposed.b, half_transposed.d),
+            c: _mm256_permute2f128_ps::<0x20>(half_transposed.e, half_transposed.g),
+            d: _mm256_permute2f128_ps::<0x20>(half_transposed.f, half_transposed.h),
+            e: _mm256_permute2f128_ps::<0x31>(half_transposed.a, half_transposed.c),
+            f: _mm256_permute2f128_ps::<0x31>(half_transposed.b, half_transposed.d),
+            g: _mm256_permute2f128_ps::<0x31>(half_transposed.e, half_transposed.g),
+            h: _mm256_permute2f128_ps::<0x31>(half_transposed.f, half_transposed.h),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub(crate) struct Dense4x4Lane<T> {
+    pub a: T,
+    pub b: T,
+    pub c: T,
+    pub d: T,
+}
+
+impl TransposeMatrix<f64> for Avx2 {
+    type RegisterMatrix = Dense4x4Lane<Self::Register>;
+
+    #[inline(always)]
+    unsafe fn load_matrix(
+        offset: usize,
+        width: usize,
+        data_ptr: *const f64,
+    ) -> Self::RegisterMatrix {
+        Dense4x4Lane {
+            a: Self::load(data_ptr.add(offset)),
+            b: Self::load(data_ptr.add(offset + (width * 1))),
+            c: Self::load(data_ptr.add(offset + (width * 2))),
+            d: Self::load(data_ptr.add(offset + (width * 3))),
+        }
+    }
+
+    #[inline(always)]
+    unsafe fn write_matrix(
+        offset: usize,
+        height: usize,
+        matrix: Self::RegisterMatrix,
+        result_ptr: *mut f64,
+    ) {
+        Self::write(result_ptr.add(offset), matrix.a);
+        Self::write(result_ptr.add(offset + (1 * height)), matrix.b);
+        Self::write(result_ptr.add(offset + (2 * height)), matrix.c);
+        Self::write(result_ptr.add(offset + (3 * height)), matrix.d);
+    }
+
+    #[inline(always)]
+    unsafe fn transpose_register_matrix(
+        matrix: Self::RegisterMatrix,
+    ) -> Self::RegisterMatrix {
+        let temp = Dense4x4Lane {
+            // Unpack the low elements of the dense lane
+            a: _mm256_unpacklo_pd(matrix.a, matrix.b),
+            b: _mm256_unpacklo_pd(matrix.c, matrix.d),
+
+            // Unpack the high elements of the dense lane
+            c: _mm256_unpackhi_pd(matrix.a, matrix.b),
+            d: _mm256_unpackhi_pd(matrix.c, matrix.d),
+        };
+
+        Dense4x4Lane {
+            a: _mm256_permute2f128_pd::<{ _MM_SHUFFLE(0, 0, 0, 2) }>(temp.b, temp.a),
+            b: _mm256_permute2f128_pd::<{ _MM_SHUFFLE(0, 0, 0, 2) }>(temp.d, temp.c),
+            c: _mm256_permute2f128_pd::<{ _MM_SHUFFLE(0, 3, 0, 1) }>(temp.a, temp.b),
+            d: _mm256_permute2f128_pd::<{ _MM_SHUFFLE(0, 3, 0, 1) }>(temp.c, temp.d),
+        }
+    }
+}
+
+#[allow(non_snake_case)]
+const fn _MM_SHUFFLE(z: u32, y: u32, x: u32, w: u32) -> i32 {
+    ((z << 6) | (y << 4) | (x << 2) | w) as i32
+}
--- cfavml-gemm/src/transpose/mod.rs
+++ cfavml-gemm/src/transpose/mod.rs
@@ -1,10 +1,13 @@
 use std::any::TypeId;
 use std::mem;
 
+use cfavml::danger::*;
+
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-mod impl_avx2_32bit;
+mod impl_avx2;
+
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-mod impl_avx2_64bit;
+pub use self::impl_avx2::*;
 
 /// Transpose a given matrix, writing the result to the given output buffer.
 pub fn transpose_matrix<T>(width: usize, height: usize, data: &[T], result: &mut [T])
@@ -36,7 +39,7 @@ where
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         unsafe {
             if is_x86_feature_detected!("avx2") {
-                impl_avx2_32bit::transpose_32bit(width, height, data, result)
+                return f32_xany_avx2_nofma_transpose(width, height, data, result)
             }
         }
     } else if TypeId::of::<T>() == TypeId::of::<f64>()
@@ -48,8 +51,167 @@ where
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         unsafe {
             if is_x86_feature_detected!("avx2") {
-                impl_avx2_64bit::transpose_64bit(width, height, data, result)
+                return f64_xany_avx2_nofma_transpose(width, height, data, result)
             }
         }
     }
+
+    // Any remaining cases falls back to a naive solution.
+    let mut j = 0;
+    while j < height {
+        let mut i = 0;
+        while i < width {
+            unsafe {
+                *result.get_unchecked_mut(i * height + j) = *data.get_unchecked(j * width + i);
+            }
+
+            i += 1;
+        }
+
+        j += 1;
+    }
+}
+
+/// Transpose a full width x height matrix.
+unsafe fn generic_transpose<T, R>(
+    width: usize,
+    height: usize,
+    data: &[T],
+    result: &mut [T],
+) where
+    T: Copy,
+    R: SimdRegister<T> + TransposeMatrix<T>,
+{
+    assert_eq!(data.len(), width * height, "Input data shape missmatch");
+    assert_eq!(
+        data.len(),
+        result.len(),
+        "Output buffer does not match input data"
+    );
+
+    let data_ptr = data.as_ptr();
+    let result_ptr = result.as_mut_ptr();
+
+    let sub_matrix_block_size = R::elements_per_lane() * 2;
+    let matrix_offset_step = R::elements_per_lane();
+
+    let width_remainder = width % sub_matrix_block_size;
+    let height_remainder = height % sub_matrix_block_size;
+
+    let mut j = 0;
+    while j < (height - height_remainder) {
+        let mut i = 0;
+        while i < (width - width_remainder) {
+            // By performing a 2Nx2N matrix transposition made up of
+            // 8x8 sub-operations we maintain cache locality as best we can
+            // Increasing the size to 4Nx4N did not appear to provide and additional
+            // benefit and was overall a net less according to benchmarks.
+
+            // top-left
+            let l1 = R::load_matrix(i + j * width, width, data_ptr);
+            let l1_transpose = R::transpose_register_matrix(l1);
+            R::write_matrix(j + i * height, height, l1_transpose, result_ptr);
+
+            // bottom-left
+            let l1 =
+                R::load_matrix(i + (j + matrix_offset_step) * width, width, data_ptr);
+            let l1_transpose = R::transpose_register_matrix(l1);
+            R::write_matrix(
+                (j + matrix_offset_step) + i * height,
+                height,
+                l1_transpose,
+                result_ptr,
+            );
+
+            // top-right
+            let l1 =
+                R::load_matrix((i + matrix_offset_step) + j * width, width, data_ptr);
+            let l1_transpose = R::transpose_register_matrix(l1);
+            R::write_matrix(
+                j + (i + matrix_offset_step) * height,
+                height,
+                l1_transpose,
+                result_ptr,
+            );
+
+            // bottom-right
+            let l1 = R::load_matrix(
+                (i + matrix_offset_step) + (j + matrix_offset_step) * width,
+                width,
+                data_ptr,
+            );
+            let l1_transpose = R::transpose_register_matrix(l1);
+            R::write_matrix(
+                (j + matrix_offset_step) + (i + matrix_offset_step) * height,
+                height,
+                l1_transpose,
+                result_ptr,
+            );
+
+            i += sub_matrix_block_size;
+        }
+
+        j += sub_matrix_block_size;
+    }
+
+    // Handles the tail of each row that does not fit within 8 wide blocks.
+    let mut j_remainder = 0;
+    while j_remainder < (height - height_remainder) {
+        let mut i = width - width_remainder;
+        while i < width {
+            *result.get_unchecked_mut(i * height + j_remainder) =
+                *data.get_unchecked(j_remainder * width + i);
+
+            i += 1;
+        }
+
+        j_remainder += 1;
+    }
+
+    // Handles the tail of each column that does not fit within 8 wide blocks.
+    while j < height {
+        let mut i = 0;
+        while i < width {
+            *result.get_unchecked_mut(i * height + j) =
+                *data.get_unchecked(j * width + i);
+
+            i += 1;
+        }
+
+        j += 1;
+    }
+}
+
+/// Generic matrix transposition.
+///
+/// The implementation of the [TransposeMatrix] operations
+/// process blocks in 4x4 sub-matrices, assuming sub-matrices will be
+/// made up of 4 [Self::RegisterMatrix] types in order to maximise
+/// cache locality.
+trait TransposeMatrix<T>: SimdRegister<T>
+where
+    T: Copy,
+{
+    /// The type representing `NxN` matrix within SIMD registers.
+    type RegisterMatrix;
+
+    /// Load a new `NxN` matrix into registers.
+    unsafe fn load_matrix(
+        offset: usize,
+        width: usize,
+        data_ptr: *const T,
+    ) -> Self::RegisterMatrix;
+
+    /// Store a `NxN` matrix in the `result_ptr` buffer.
+    unsafe fn write_matrix(
+        offset: usize,
+        height: usize,
+        matrix: Self::RegisterMatrix,
+        result_ptr: *mut T,
+    );
+
+    /// Transpose the [Self::RegisterMatrix] instance returning the transposed data.
+    unsafe fn transpose_register_matrix(
+        matrix: Self::RegisterMatrix,
+    ) -> Self::RegisterMatrix;
 }
