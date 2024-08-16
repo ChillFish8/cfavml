e858ae44445a6610a1fbf5101ade07a6b4ea301c
chillfish8 <hburt2003@gmail.com>
2024-08-16 17:31:25 +0100

    Start fixing benches

--- cfavml/benches/bench_distance_measures.rs
+++ cfavml/benches/bench_distance_measures.rs
@@ -3,28 +3,40 @@ extern crate blas_src;
 
 use std::hint::black_box;
 
-use cfavml::math::*;
+use divan::counter::ItemsCount;
 use divan::Bencher;
 use ndarray::ArrayView1;
 
 mod utils;
 
-const DIMS: usize = 1341;
+const DIMS: usize = 1536;
 
 fn main() {
     divan::main();
 }
 
-#[cfg_attr(
-    not(debug_assertions),
-    divan::bench_group(sample_count = 500, sample_size = 2500, threads = false)
+#[divan::bench_group(
+    sample_count = 500,
+    sample_size = 5000,
+    threads = false,
+    counters = [ItemsCount::new(DIMS)],
 )]
-mod dot_product_x1341 {
+mod dot_product {
+    use cfavml::safe_trait_distance_ops::DistanceOps;
+    use ndarray::linalg::Dot;
+    use ndarray::{Data, ViewRepr};
+    use rand::distributions::{Distribution, Standard};
+
     use super::*;
 
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn ndarray_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
+    #[divan::bench(types = [f32, f64, i8, i16, i32, i64, u8, u16, u32, u64])]
+    fn ndarray<T>(bencher: Bencher)
+    where
+        Standard: Distribution<T>,
+        for<'a> ViewRepr<&'a mut T>: Data<Elem = T>,
+        for<'a> ArrayView1<'a, T>: Dot<ArrayView1<'a, T>>,
+    {
+        let (l1, l2) = utils::get_sample_vectors::<T>(DIMS);
         let l1_view = ArrayView1::from_shape((l1.len(),), &l1).unwrap();
         let l2_view = ArrayView1::from_shape((l2.len(),), &l2).unwrap();
 
@@ -36,79 +48,48 @@ mod dot_product_x1341 {
         });
     }
 
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn ndarray_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-        let l1_view = ArrayView1::from_shape((l1.len(),), &l1).unwrap();
-        let l2_view = ArrayView1::from_shape((l2.len(),), &l2).unwrap();
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(l1_view);
-            let l2_view = black_box(l2_view);
-
-            l1_view.dot(&l2_view)
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn simsimd_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(&l1);
-            let l2_view = black_box(&l2);
-
-            simsimd::SpatialSimilarity::dot(l1_view, l2_view).unwrap_or_default()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn simsimd_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(&l1);
-            let l2_view = black_box(&l2);
-
-            simsimd::SpatialSimilarity::dot(l1_view, l2_view).unwrap_or_default()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn cfavml_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
+    #[divan::bench(types = [f32, f64, i8, i16, i32, i64, u8, u16, u32, u64])]
+    fn cfavml<T>(bencher: Bencher)
+    where
+        Standard: Distribution<T>,
+        T: DistanceOps,
+    {
+        let (l1, l2) = utils::get_sample_vectors::<T>(DIMS);
 
         bencher.bench_local(|| {
             let l1_view = black_box(l1.as_ref());
             let l2_view = black_box(l2.as_ref());
 
-            cfavml::f32_xany_dot(l1_view, l2_view)
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn cfavml_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(l1.as_ref());
-            let l2_view = black_box(l2.as_ref());
-
-            cfavml::f64_xany_dot(l1_view, l2_view)
+            cfavml::dot(l1_view, l2_view)
         });
     }
 }
 
-#[cfg_attr(
-    not(debug_assertions),
-    divan::bench_group(sample_count = 500, sample_size = 2500, threads = false)
+#[divan::bench_group(
+    sample_count = 500,
+    sample_size = 5000,
+    threads = false,
+    counters = [ItemsCount::new(DIMS)],
 )]
-mod cosine_x1341 {
+mod cosine {
+    use cfavml::math::*;
+    use cfavml::safe_trait_distance_ops::DistanceOps;
+    use ndarray::linalg::Dot;
+    use ndarray::{Data, ViewRepr};
+    use rand::distributions::{Distribution, Standard};
+
     use super::*;
 
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn ndarray_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
+    #[divan::bench(types = [f32, f64])]
+    fn ndarray<T>(bencher: Bencher)
+    where
+        T: Copy + num_traits::identities::One,
+        AutoMath: Math<T>,
+        Standard: Distribution<T>,
+        for<'a> ViewRepr<&'a mut T>: Data<Elem = T>,
+        for<'a> ArrayView1<'a, T>: Dot<ArrayView1<'a, T>, Output = T>,
+    {
+        let (l1, l2) = utils::get_sample_vectors::<T>(DIMS);
         let l1_view = ArrayView1::from_shape((l1.len(),), &l1).unwrap();
         let l2_view = ArrayView1::from_shape((l2.len(),), &l2).unwrap();
 
@@ -123,156 +104,76 @@ mod cosine_x1341 {
         });
     }
 
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn ndarray_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-        let l1_view = ArrayView1::from_shape((l1.len(),), &l1).unwrap();
-        let l2_view = ArrayView1::from_shape((l2.len(),), &l2).unwrap();
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(l1_view);
-            let l2_view = black_box(l2_view);
-
-            let norm_l1 = l1_view.product();
-            let norm_l2 = l2_view.product();
-            let dot = l1_view.dot(&l2_view);
-            utils::cosine::<_, AutoMath>(dot, norm_l1, norm_l2)
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn simsimd_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(&l1);
-            let l2_view = black_box(&l2);
-
-            simsimd::SpatialSimilarity::cosine(l1_view, l2_view).unwrap_or_default()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn simsimd_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(&l1);
-            let l2_view = black_box(&l2);
-
-            simsimd::SpatialSimilarity::cosine(l1_view, l2_view).unwrap_or_default()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn cfavml_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
+    #[divan::bench(types = [f32, f64])]
+    fn cfavml<T>(bencher: Bencher)
+    where
+        Standard: Distribution<T>,
+        T: DistanceOps,
+    {
+        let (l1, l2) = utils::get_sample_vectors::<T>(DIMS);
 
         bencher.bench_local(|| {
             let l1_view = black_box(l1.as_ref());
             let l2_view = black_box(l2.as_ref());
 
-            cfavml::f32_xany_cosine(l1_view, l2_view)
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn cfavml_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(l1.as_ref());
-            let l2_view = black_box(l2.as_ref());
-
-            cfavml::f64_xany_cosine(l1_view, l2_view)
+            cfavml::cosine(l1_view, l2_view)
         });
     }
 }
 
-#[cfg_attr(
-    not(debug_assertions),
-    divan::bench_group(sample_count = 500, sample_size = 2500, threads = false)
+#[divan::bench_group(
+    sample_count = 500,
+    sample_size = 5000,
+    threads = false,
+    counters = [ItemsCount::new(DIMS)],
 )]
-mod euclidean_x1341 {
+mod euclidean {
     use std::ops::Sub;
 
+    use cfavml::math::*;
+    use cfavml::safe_trait_distance_ops::DistanceOps;
+    use ndarray::linalg::Dot;
+    use ndarray::{Array1, Data, OwnedRepr};
+    use rand::distributions::{Distribution, Standard};
+
     use super::*;
 
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn ndarray_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
-        let l1_view = ArrayView1::from_shape((l1.len(),), &l1).unwrap();
-        let l2_view = ArrayView1::from_shape((l2.len(),), &l2).unwrap();
+    #[divan::bench(types = [f32, f64])]
+    fn ndarray<T>(bencher: Bencher)
+    where
+        T: Copy + num_traits::identities::One,
+        AutoMath: Math<T>,
+        Standard: Distribution<T>,
+        OwnedRepr<T>: Data<Elem = T>,
+        Array1<T>: Dot<Array1<T>, Output = T>,
+        for<'a> &'a Array1<T>: Sub<&'a Array1<T>, Output = Array1<T>>,
+    {
+        let (l1, l2) = utils::get_sample_vectors::<T>(DIMS);
+        let l1_view = Array1::from_shape_vec((l1.len(),), l1.to_vec()).unwrap();
+        let l2_view = Array1::from_shape_vec((l2.len(),), l2.to_vec()).unwrap();
 
         bencher.bench_local(|| {
-            let l1_view = black_box(l1_view);
-            let l2_view = black_box(l2_view);
+            let l1_view = black_box(&l1_view);
+            let l2_view = black_box(&l2_view);
 
-            let diff = l1_view.sub(&l2_view);
-            diff.product()
+            let diff = l1_view - l2_view;
+            diff.dot(&diff)
         });
     }
 
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn ndarray_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-        let l1_view = ArrayView1::from_shape((l1.len(),), &l1).unwrap();
-        let l2_view = ArrayView1::from_shape((l2.len(),), &l2).unwrap();
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(l1_view);
-            let l2_view = black_box(l2_view);
-
-            let diff = l1_view.sub(&l2_view);
-            diff.product()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn simsimd_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(&l1);
-            let l2_view = black_box(&l2);
-
-            simsimd::SpatialSimilarity::sqeuclidean(l1_view, l2_view).unwrap_or_default()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn simsimd_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(&l1);
-            let l2_view = black_box(&l2);
-
-            simsimd::SpatialSimilarity::sqeuclidean(l1_view, l2_view).unwrap_or_default()
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn cfavml_f32(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f32>(DIMS);
+    #[divan::bench(types = [f32, f64])]
+    fn cfavml<T>(bencher: Bencher)
+    where
+        Standard: Distribution<T>,
+        T: DistanceOps,
+    {
+        let (l1, l2) = utils::get_sample_vectors::<T>(DIMS);
 
         bencher.bench_local(|| {
             let l1_view = black_box(l1.as_ref());
             let l2_view = black_box(l2.as_ref());
 
-            cfavml::f32_xany_squared_euclidean(l1_view, l2_view)
-        });
-    }
-
-    #[cfg_attr(not(debug_assertions), divan::bench)]
-    fn cfavml_f64(bencher: Bencher) {
-        let (l1, l2) = utils::get_sample_vectors::<f64>(DIMS);
-
-        bencher.bench_local(|| {
-            let l1_view = black_box(l1.as_ref());
-            let l2_view = black_box(l2.as_ref());
-
-            cfavml::f64_xany_squared_euclidean(l1_view, l2_view)
+            cfavml::cosine(l1_view, l2_view)
         });
     }
 }
--- cfavml/benches/utils.rs
+++ cfavml/benches/utils.rs
@@ -1,3 +1,4 @@
+use cfavml::math::Math;
 use rand::distributions::Standard;
 use rand::prelude::Distribution;
 use rand::{Rng, SeedableRng};
@@ -16,8 +17,6 @@ macro_rules! repeat {
         }
     }};
 }
-
-#[cfg(not(feature = "benchmark-aligned"))]
 pub fn get_sample_vectors<T>(size: usize) -> (Vec<T>, Vec<T>)
 where
     Standard: Distribution<T>,
@@ -32,95 +31,6 @@ where
     }
 
     (x, y)
-}
-
-#[cfg(feature = "benchmark-aligned")]
-pub fn get_sample_vectors<T>(
-    size: usize,
-) -> (aligned::AlignedBuffer<T>, aligned::AlignedBuffer<T>)
-where
-    Standard: Distribution<T>,
-{
-    use std::ops::DerefMut;
-
-    let mut rng = ChaCha8Rng::seed_from_u64(SEED);
-
-    let mut x = aligned::AlignedBuffer::new(size);
-    let mut y = aligned::AlignedBuffer::new(size);
-
-    let x_buf = x.deref_mut();
-    let y_buf = y.deref_mut();
-    for i in 0..size {
-        x_buf[i] = rng.gen();
-        y_buf[i] = rng.gen();
-    }
-
-    (x, y)
-}
-
-use cfavml::math::Math;
-
-#[cfg(feature = "benchmark-aligned")]
-pub mod aligned {
-    use std::fmt::Debug;
-    use std::iter::repeat;
-    use std::marker::PhantomData;
-    use std::mem;
-    use std::ops::{Deref, DerefMut};
-
-    #[derive(Debug, Default, Copy, Clone)]
-    #[repr(C, align(64))]
-    struct AlignTo64([u64; 8]);
-
-    pub struct AlignedBuffer<T> {
-        size: usize,
-        phantom: PhantomData<T>,
-        inner: Vec<AlignTo64>,
-    }
-
-    impl<T> AlignedBuffer<T> {
-        pub fn new(size: usize) -> Self {
-            Self {
-                size,
-                phantom: PhantomData,
-                inner: aligned_vec::<T>(size),
-            }
-        }
-    }
-
-    impl<T> AsRef<[T]> for AlignedBuffer<T> {
-        fn as_ref(&self) -> &[T] {
-            self.deref()
-        }
-    }
-
-    impl<T> Deref for AlignedBuffer<T> {
-        type Target = [T];
-
-        fn deref(&self) -> &Self::Target {
-            let ptr = self.inner.as_ptr();
-            unsafe { std::slice::from_raw_parts::<T>(ptr.cast(), self.size) }
-        }
-    }
-
-    impl<T> DerefMut for AlignedBuffer<T> {
-        fn deref_mut(&mut self) -> &mut Self::Target {
-            let ptr = self.inner.as_mut_ptr();
-            unsafe { std::slice::from_raw_parts_mut::<T>(ptr.cast(), self.size) }
-        }
-    }
-
-    fn aligned_vec<T>(n_elements: usize) -> Vec<AlignTo64> {
-        let chunk_size = 64 / mem::size_of::<T>();
-
-        // Lazy math to ensure we always have enough.
-        let n_units = (n_elements / chunk_size) + 1;
-
-        let mut aligned: Vec<AlignTo64> = Vec::with_capacity(n_units);
-        aligned.extend(repeat(AlignTo64::default()).take(n_units));
-
-        aligned
-    }
 }
 
 #[allow(unused)]
